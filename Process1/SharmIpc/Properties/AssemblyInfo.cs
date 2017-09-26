45c85b3296ec71de96e9e3a5786c7876e72e7da6
Alexey Solovyov <hhblaze@gmail.com>
2017-09-26 11:58:23 +0200

    Release v1.15

--- Process1/SharmIpc/Properties/AssemblyInfo.cs
+++ Process1/SharmIpc/Properties/AssemblyInfo.cs
@@ -32,7 +32,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.14.2017.0910")]
-[assembly: AssemblyFileVersion("1.14.2017.0910")]
+[assembly: AssemblyVersion("1.15.2017.0926")]
+[assembly: AssemblyFileVersion("1.15.2017.0926")]
 
 //[assembly: AssemblyFileVersion("1.7.2015.0827")]
